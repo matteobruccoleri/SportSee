9048a25e5e66196c1986c27d2014931d2e181c0b
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2025-05-02 17:28:39 +0200

    feat: suppr comment;

--- src/models/SessionModel.js
+++ src/models/SessionModel.js
@@ -2,7 +2,6 @@ class SessionModel {
   constructor(data) {
     const dayLabels = { 1: 'L', 2: 'M', 3: 'M', 4: 'J', 5: 'V', 6: 'S', 7: 'D' };
     
-    // Retourner directement un tableau pour le LineChart
     if (Array.isArray(data)) {
       this.data = data.map(session => ({
         day: dayLabels[session.day] || session.day.toString(),
